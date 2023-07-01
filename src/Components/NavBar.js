611e72c891480cd0cdac0e55bfed2ac2e9decb61
aditya singh <adisingh925gmail.com>
2023-07-01 19:38:42 +0530

    minor changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext} from "react";
 import { Link, useLocation } from "react-router-dom";
 import globalContext from "../Context/GlobalContext";
 import Button from "@mui/material/Button";
